19aef1be98623d672a7fee04eefdf95c984d1910
Edgar Mendez <edgar.ym26@gmail.com>
2022-05-26 13:18:24 -0400

    added event list to scroll wheel

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,3 +169,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+const element = document.querySelector("#container");
+
+element.addEventListener('wheel', (event) => {
+  event.preventDefault();
+
+  element.scrollBy({
+    left: event.deltaY < 0 ? -30 : 30,
+    
+  });
+});
