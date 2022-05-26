d430eedb3fe2617316835c3b1f9d91f123bd212b
Edgar Mendez <edgar.ym26@gmail.com>
2022-05-26 15:14:57 -0400

    added 1 new img for top 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,14 +169,3 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
-
-const element = document.querySelector("#container");
-
-element.addEventListener('wheel', (event) => {
-  event.preventDefault();
-
-  element.scrollBy({
-    left: event.deltaY < 0 ? -30 : 30,
-    
-  });
-});
